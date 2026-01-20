97db4eeb5cb41f4d79f8568660c6ed44e6754e70
Ayush Saha <ayushsaha.work@gmail.com>
2026-01-20 22:07:31 +0530

    Add nextLargerElement function in Solution class
    Implement nextLargerElement function to find the next greater element for each element in the array.

---
+++ Stack/Next Greater Element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
+         int n = arr.size();
+        vector<int> ans(n,-1);
+        stack<int>st;
+        for(int i = 0; i < n ; i++){
+            while(!st.empty() && arr[i] > arr[st.top()]){
+                ans[st.top()] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
